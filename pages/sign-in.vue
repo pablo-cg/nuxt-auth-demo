4dd348826bb5abbefee5a1f16a024243a8c260a2
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-12-04 14:32:38 -0300

    feat: logica para iniciar sesion y middleware solo para invitados

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -1,4 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['only-guests'],
+});
+
+const { toastSuccess, toastError } = useToaster();
+
+const form = reactive({
+  email: '',
+  password: '',
+});
+
+async function signIn() {
+  try {
+    await $fetch('/auth/signin', {
+      method: 'POST',
+      body: form,
+    });
+
+    toastSuccess('You are now signed in');
+
+    await navigateTo('/');
+  } catch (error: any) {
+    toastError(error?.statusMessage);
+  }
+}
+</script>
 
 <template>
   <main class="container mx-auto relative">
@@ -6,11 +32,27 @@
       <h1 class="text-3xl font-bold">Sign In</h1>
       <div class="flex gap-2">
         <UButton to="/">Home</UButton>
-        <UButton to="/">Sign Out</UButton>
+        <UButton to="/sign-up">Sign Up</UButton>
       </div>
     </section>
-    <section class="flex justify-center items-center h-screen">
-      <form></form>
+    <section class="flex flex-col gap-5 justify-center items-center h-screen">
+      <h2 class="text-xl font-bold">Sign In</h2>
+      <form
+        @submit.prevent="signIn"
+        class="flex flex-col gap-3 w-1/3"
+      >
+        <UInput
+          type="email"
+          placeholder="Email"
+          v-model="form.email"
+        />
+        <UInput
+          type="password"
+          placeholder="Password"
+          v-model="form.password"
+        />
+        <UButton type="submit">Sign In</UButton>
+      </form>
     </section>
   </main>
 </template>
