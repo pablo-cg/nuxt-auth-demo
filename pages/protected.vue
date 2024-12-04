783874c78c8fba03d6ab4da85aa8b7474a2c8db2
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-12-04 14:31:41 -0300

    feat: agregada logica para cerrar sesion y activado el middleware protected

--- pages/protected.vue
+++ pages/protected.vue
@@ -1,19 +1,42 @@
 <script setup lang="ts">
 const user = useUser();
+const { toastInfo } = useToaster();
 
 definePageMeta({
-  // middleware: ['protected'],
+  middleware: ['protected'],
 });
+
+async function signOut() {
+  try {
+    await $fetch('/auth/signout', {
+      method: 'POST',
+    });
+
+    toastInfo('You are now signed out');
+
+    await navigateTo('/');
+  } catch (error) {
+    console.log(error);
+  }
+}
 </script>
 
 <template>
   <main class="container mx-auto relative">
-    <h1 class="text-3xl font-bold my-4 absolute top-0">Protected Page</h1>
+    <section class="my-4 absolute top-0 w-full flex justify-between items-center">
+      <h1 class="text-3xl font-bold">Protected</h1>
+      <div class="flex gap-2">
+        <UButton to="/">Home</UButton>
+      </div>
+    </section>
     <section class="flex flex-col gap-3 justify-center items-center h-screen">
       <p class="text-xl">You are logged in</p>
+      <p>
+        <pre>{{ user }}</pre>
+      </p>
       <UButton
         size="lg"
-        to="/sign-in"
+        @click="signOut"
       >
         Sign Out
       </UButton>
